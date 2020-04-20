8912ea0964e8d4dbd4a1e44b918f9e1e93d37d84
Renata Marques <renata.nrmarques@gmail.com>
2020-04-20 12:52:12 -0300

    removing database default

--- db/schema.rb
+++ db/schema.rb
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 2020_04_18_161451) do
   end
 
   create_table "venues", force: :cascade do |t|
-    t.string "name", default: "Unkown", null: false
-    t.integer "rows", default: 0, null: false
-    t.integer "columns", default: 0, null: false
+    t.string "name", null: false
+    t.integer "rows", null: false
+    t.integer "columns", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
