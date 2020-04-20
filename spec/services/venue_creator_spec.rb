27a066769d987b1e35e69978b8a8d462dff46b8f
Renata Marques <renata.nrmarques@gmail.com>
2020-04-20 00:00:43 -0300

    service spec

--- spec/services/venue_creator_spec.rb
+++ spec/services/venue_creator_spec.rb
@@ -5,8 +5,22 @@ require 'rails_helper'
 RSpec.describe VenueCreator, type: :service do
   describe 'creates venue with seats' do
     context 'creates venue successfully' do
-      it '' do
-        # expect
+      let(:valid_venue) do
+        FactoryBot.attributes_for(:venue)
+      end
+
+      it '.perform' do
+        expect(described_class.new(params: valid_venue).perform).to eq(1)
+      end
+    end
+
+    context 'do not create venue' do
+      let(:invalid_venue) do
+        { rows: 1, columns: 1 }
+      end
+
+      it '.perform' do
+        expect(described_class.new(params: invalid_venue).perform).to eq(false)
       end
     end
   end
