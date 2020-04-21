d2a4ef8d778052451a435425df30bd59563b6c0e
Renata Marques <renata.nrmarques@gmail.com>
2020-04-21 20:18:44 -0300

    specs and finder class added

--- spec/requests/best_seats_controller_spec.rb
+++ spec/requests/best_seats_controller_spec.rb
@@ -7,23 +7,10 @@ RSpec.describe 'BestSeatsController', type: :request do
     context 'get best seat successfully' do
       let(:message) do
         {
-          "data" => true,
+          "data" => [],
           "message" => I18n.t('best_seat.valid'),
           "status" => I18n.t('status.success')
         }
-      end
-
-      let(:expected) do
-        []
-      end
-
-      xit 'get best seat' do
-        get '/api/best_seats'
-        expect(JSON.parse(response.body)).to include(expected)
-      end
-
-      xit 'get best seats' do
-
       end
 
       it 'get best seat with status code 200' do
@@ -31,7 +18,7 @@ RSpec.describe 'BestSeatsController', type: :request do
         expect(response).to have_http_status(200)
       end
 
-      xit 'render successful message' do
+      it 'render successful message' do
         get '/api/best_seats'
         expect(JSON.parse(response.body)).to eq(message)
       end
